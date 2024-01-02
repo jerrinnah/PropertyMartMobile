a5f1e9cf0ce5f746b4c4345602222a9d55f5e42c
xenxei46 <wisdomjohnikoi@gmail.com>
2024-01-02 12:39:28 +0100

    merged

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,13 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import Box from '@component/general/Box'
 import CustomText from '@component/general/CustomText'
 import useForm from '@hooks/useForm'
 import { loginSchema } from '@services/validation'
+import { Styles } from './styles'
 import { CustomTextInput } from '@component/form/CustomInput'
 import { Link } from 'expo-router';
 import { SubmitButton } from '@component/form/CustomButton'
 import { Separator } from 'tamagui'
+const logo = require('../../assets/images/logo/logo.png')
 
 const Login = () => {
   const { renderForm, formState: { isValid } } = useForm({
@@ -18,37 +20,51 @@ const Login = () => {
     validationSchema: loginSchema,
   })
   return renderForm(
-    <Box flex={1} backgroundColor={'background'} padding={'md'}>
-      {/* IMAGE BOX */}
-      <Box width='100%' height={150} bg='background' />
-      <CustomText variant={'subheader'}>Welcome Back</CustomText>
-      <CustomText variant={'xs'} marginTop={'md'} marginBottom={'3xl'}>Enter your email address and password to gain access.</CustomText>
+    // <Box flex={1} backgroundColor={'background'} padding={'md'}>
+    //   <Box height={10} width={'100%'} backgroundColor={'errorColor'}>
+    //     {/* <Image source={logo} resizeMode="cover" /> */}
+    //   </Box>
+    //   <Box width='100%' height={100} bg='background' />
+    //   <CustomText variant={'subheader'}>Welcome Back</CustomText>
+    //   <CustomText variant={'xs'} marginTop={'md'} marginBottom={'3xl'}>Enter your email address and password to gain access.</CustomText>
 
-      <CustomTextInput name='email' placeholder='Email' label='Email Address' isPassword={false} />
-      <Box marginBottom={'xl'} />
-      <CustomTextInput name='password' placeholder='Password' label='Password' isPassword />
+    //   <CustomTextInput name='email' placeholder='Email' label='Email Address' isPassword={false} />
+    //   <Box marginBottom={'xl'} />
+    //   <CustomTextInput name='password' placeholder='Password' label='Password' isPassword />
 
-      <Box width='100%' marginBottom={'md'} height={40} justifyContent={'center'} alignItems={'flex-end'}>
-        <Link href="/auth/forgotpassword">
-          <CustomText variant={'xs'} color={'primaryColor'}>Forgot Passwrd?</CustomText>
-        </Link>
-      </Box>
+    //   <Box width='100%' marginBottom={'md'} height={40} justifyContent={'center'} alignItems={'flex-end'}>
+    //     <Link href="/auth/forgotpassword">
+    //       <CustomText variant={'xs'} color={'primaryColor'}>Forgot Passwrd?</CustomText>
+    //     </Link>
+    //   </Box>
 
-      <SubmitButton label='Log in' width='100%'  onSubmit={() => {}} />
+    //   <SubmitButton label='Log in' width='100%'  onSubmit={() => {}} />
       
-      <Box width='100%' flexDirection={'row'} height={60} alignItems={'center'} >
-        <Separator />
-        <CustomText variant={'body'} color={'black'}>OR</CustomText>
-        <Separator />
-      </Box>
+    //   <Box width='100%' flexDirection={'row'} height={60} alignItems={'center'} >
+    //     <Separator />
+    //     <CustomText variant={'body'} color={'black'}>OR</CustomText>
+    //     <Separator />
+    //   </Box>
 
-      <Box flexDirection={'row'} width='100%' justifyContent={'center'} alignItems={'center'}>
-        <CustomText variant={'xs'}>Don't have an account?</CustomText>
-        <Link href={'/auth/signup'} style={{ marginLeft: 4 }}>
-          <CustomText variant={'body'} color={'primaryColor'} marginLeft={'xs'}>Create Account</CustomText>
-        </Link>
-      </Box>
+    //   <Box flexDirection={'row'} width='100%' justifyContent={'center'} alignItems={'center'}>
+    //     <CustomText variant={'xs'}>Don't have an account?</CustomText>
+    //     <Link href={'/auth/signup'} style={{ marginLeft: 4 }}>
+    //       <CustomText variant={'body'} color={'primaryColor'} marginLeft={'xs'}>Create Account</CustomText>
+    //     </Link>
+    //   </Box>
 
+    // </Box>
+    <Box style={[Styles.martContainer, Styles.flex]} >
+      <Box style={Styles.subContainer} >
+        <Box height={'60%'} backgroundColor={'primaryColor'}>
+          <Box height={'10%'} width={'100%'}>
+            <Image source={logo} resizeMode="cover" style={Styles.logo} />
+          </Box>
+        </Box>
+        <Box height={'40%'}>
+
+        </Box>
+      </Box>
     </Box>
   )
 }
--- app/auth/styles.ts
+++ app/auth/styles.ts
@@ -9,7 +9,7 @@ export const Styles = StyleSheet.create({
    },
    subContainer: {
     height:'90%',
-    width: '90%',
+    width: '95%',
    },
 
    flex: {
@@ -17,58 +17,11 @@ export const Styles = StyleSheet.create({
     alignItems:'center',
     justifyContent:'center'
    },
-
-   martSplit60: {
-    height: '60%',
-    width:'100%'
-   },
-   martSplit55: {
-    height: '55%',
-    width:'100%'
-   },
-   martSplit50: {
-    height: '50%',
-    width:'100%'
-   },
-   martSplit48: {
-    height: '48%',
-    width:'100%'
-   },
-   martSplit45: {
-    height: '45%',
-    width:'100%'
-   },
-   martSplit40: {
-    height: '40%',
-    width:'100%'
-   },
-
-   bgImage: {
-    height: '100%',
-    width:'100%'
-   },
-
-   logoContainer: {
-    display: 'flex',
-    justifyContent:'flex-end',
-    alignItems:'center',
-    height:'100%',
-   },
-   logoBox: {
-    display: 'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:'white',
-    width:'20%',
-    height:'20%',
-    borderRadius: 20,
-    marginBottom:10,
-   },
-   logoImage: {
-    objectFit:'contain',
-    width:40,
-    height:40
-   },
+   
+   logo: {
+      width:100,
+      height:60
+   }
 
 
 
