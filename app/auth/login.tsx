e2b2145d9e55076df8c0b98c095e9ef05527988c
xenxei46 <wisdomjohnikoi@gmail.com>
2024-01-05 16:10:26 +0100

    stashed changes

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -20,49 +20,54 @@ const Login = () => {
     validationSchema: loginSchema,
   })
   return renderForm(
-    // <Box flex={1} backgroundColor={'background'} padding={'md'}>
-    //   <Box height={10} width={'100%'} backgroundColor={'errorColor'}>
-    //     {/* <Image source={logo} resizeMode="cover" /> */}
-    //   </Box>
-    //   <Box width='100%' height={100} bg='background' />
-    //   <CustomText variant={'subheader'}>Welcome Back</CustomText>
-    //   <CustomText variant={'xs'} marginTop={'md'} marginBottom={'3xl'}>Enter your email address and password to gain access.</CustomText>
-
-    //   <CustomTextInput name='email' placeholder='Email' label='Email Address' isPassword={false} />
-    //   <Box marginBottom={'xl'} />
-    //   <CustomTextInput name='password' placeholder='Password' label='Password' isPassword />
-
-    //   <Box width='100%' marginBottom={'md'} height={40} justifyContent={'center'} alignItems={'flex-end'}>
-    //     <Link href="/auth/forgotpassword">
-    //       <CustomText variant={'xs'} color={'primaryColor'}>Forgot Passwrd?</CustomText>
-    //     </Link>
-    //   </Box>
-
-    //   <SubmitButton label='Log in' width='100%'  onSubmit={() => {}} />
-      
-    //   <Box width='100%' flexDirection={'row'} height={60} alignItems={'center'} >
-    //     <Separator />
-    //     <CustomText variant={'body'} color={'black'}>OR</CustomText>
-    //     <Separator />
-    //   </Box>
-
-    //   <Box flexDirection={'row'} width='100%' justifyContent={'center'} alignItems={'center'}>
-    //     <CustomText variant={'xs'}>Don't have an account?</CustomText>
-    //     <Link href={'/auth/signup'} style={{ marginLeft: 4 }}>
-    //       <CustomText variant={'body'} color={'primaryColor'} marginLeft={'xs'}>Create Account</CustomText>
-    //     </Link>
-    //   </Box>
-
-    // </Box>
+  
     <Box style={[Styles.martContainer, Styles.flex]} >
-      <Box style={Styles.subContainer} >
-        <Box height={'60%'} backgroundColor={'primaryColor'}>
-          <Box height={'10%'} width={'100%'}>
+      <Box style={Styles.subContainer}  marginTop={'lg'}>
+        <Box height={'80%'} borderColor={'successColor'}>
+          <Box height={'100%'} width={'100%'}>
+            
             <Image source={logo} resizeMode="cover" style={Styles.logo} />
+            <CustomText variant={'subheader'} textAlign={'left'} fontSize={26} lineHeight={25} marginTop={'md'} 
+                  color={'black'} fontWeight={'800'}>Welcome Back
+            </CustomText>
+            <CustomText variant={'xs'} textAlign={'left'} fontSize={12} lineHeight={25} 
+                  color={'black'} fontWeight={'400'}>Enter your email address and password to gain access.
+            </CustomText>
+
+            <Box marginTop={'xl'}>
+                <CustomTextInput name='email' placeholder='Email' label='Email Address' isPassword={false}  />
+                  <Box marginBottom={'xl'} />
+                <CustomTextInput name='password' placeholder='Password' label='Password' isPassword />
+            </Box>
+
+            <Box width='100%' marginBottom={'md'} height={40} justifyContent={'center'} alignItems={'flex-end'}>
+           <Link href="/auth/forgotpassword">
+             <CustomText variant={'xs'} color={'primaryColor'} fontSize={12} fontWeight={'800'} >Forgot Password?</CustomText>
+           </Link>
+           </Box>
+           <SubmitButton label='Log in' width='100%'  onSubmit={() => {}} />
+
+           <Box width='100%' flexDirection={'row'} height={60} alignItems={'center'} >
+           <Separator />
+          <CustomText variant={'xs'} fontSize={12} fontWeight={'800'} color={'black'}>OR</CustomText>
+          <Separator />
+         </Box>
+
+         <Box flexDirection={'row'} width='100%' justifyContent={'center'} alignItems={'center'}>
+           <CustomText variant={'xs'} fontSize={12}>Don't have an account?</CustomText>
+           <Link href={'/auth/signup'} style={{ marginLeft: 4 }}>
+           <CustomText variant={'body'} fontSize={12} fontWeight={'800'} color={'primaryColor'} marginLeft={'xs'}>Create Account</CustomText>
+           </Link>
+         </Box>
+
           </Box>
         </Box>
-        <Box height={'40%'}>
-
+        <Box height={'20%'} flexDirection={'row'} alignItems={'flex-end'}>
+                <Box height={5} width={'100%'}  flexDirection={'row'} justifyContent={'center'} >
+                    <Box height={5} width={'30%'} backgroundColor={'black'} borderRadius={10}>
+                        <CustomText>Hello</CustomText>
+                    </Box>
+                </Box>
         </Box>
       </Box>
     </Box>
